4ed24c92a76101f0353764d512229f5762d5e912
lch-98 <dlcndgusgnss@naver.com>
2025-06-13 16:48:04 +0900

    update font.c

--- src/picow_lcd/main.c
+++ src/picow_lcd/main.c
@@ -7,9 +7,10 @@ int main() {
     lcd_init();
     lcd_fill_screen(rgb565(255, 255, 255)); // 흰색
 
-    lcd_draw_text_mixed(5, 10, "기온 : N/A℃ ", rgb565(25, 25, 112), rgb565(255, 255, 255));
-    lcd_draw_text_mixed(5, 30, "날씨 : N/A", rgb565(25, 25, 112), rgb565(255, 255, 255));
-    lcd_draw_signnum_string_scaled(4, 53, "23:55", rgb565(25, 25, 112), rgb565(255, 255, 255), 3);
+    lcd_draw_text_mixed(5, 5, "기온 : 25.9℃ ", rgb565(25, 25, 112), rgb565(255, 255, 255));
+    lcd_draw_text_mixed(5, 22, "날씨 : 눈날림", rgb565(25, 25, 112), rgb565(255, 255, 255));
+    lcd_draw_text_mixed(30, 43, "25/06/13", rgb565(25, 25, 112), rgb565(255, 255, 255));
+    lcd_draw_signnum_string_scaled(2, 58, "16:45", rgb565(25, 25, 112), rgb565(255, 255, 255), 3);
     lcd_draw_bitmap_scaled(40, 105, 50, 50, umbrella_bitmap, 1);
 
     while (1);
